6e9818707e0b01372b1b4589a0a9e06ec39ac1a1
suoglu <yigitsuoglu@icloud.com>
2020-11-21 01:29:14 +0100

    debouncer module added

---
+++ Modules/btn_debouncer.v
@@ -0,0 +1,24 @@
+// Yigit Suoglu
+// Contains debouncer module for buttons
+
+module debouncer(clk, rst, in_n, out_c);
+  input clk, rst, in_n;
+  output out_c;
+
+  reg [1:0] mid;
+
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        begin
+          mid <= 2'b0;
+        end
+      else
+        begin
+          mid <= {mid[0], in_n};
+        end
+    end
+
+  assign out_c = (~mid[1]) & mid[0]; //rising edge
+
+endmodule // debouncer
